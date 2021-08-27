446b7cc85b6735e6fc9dcb211caf4171cfd4e728
eliaraguso <elia.raguso@gmail.com>
2021-08-27 15:26:58 +0200

    corretto snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,27 +1,47 @@
-// Il software deve chiedere per 5 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
-// Esegui questo programma in due versioni, con il for e con il while.
+// // Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// // Il programma stampa la somma di tutti i numeri inseriti.
+// // Esegui questo programma in due versioni, con il for e con il while.
 
 
-// -----------------------------FOR------------------------------
+// // -----------------------------FOR------------------------------
 
-// Creo variabile che conterrà la somma
-var somma = 0;
+// // Creo variabile che conterrà la somma
+// var somma = 0;
 
-// Ciclo per 5 volte
-for (var i = 0; i < 5; i++) {
-    // chiedo il numero all'utente
-    var numero = parseInt(prompt("Inserisci il "+ (i+1) + " numero: "));
-    // valido l'input dell'utente
-    while ( isNaN(numero)) {
-        numero = parseInt(prompt("Attento! quello che hai inserito non è un numero!!"))
-    }
-    // sommo il numero alla somma precedente
-    somma += numero;
-}
-// stampo la somma
-alert("ciao la tua somma è " + somma)
+// // Ciclo per 5 volte
+// for (var i = 0; i < 5; i++) {
+//     // chiedo il numero all'utente
+//     var numero = parseInt(prompt("Inserisci il "+ (i+1) + " numero: "));
+//     // valido l'input dell'utente
+//     while ( isNaN(numero)) {
+//         numero = parseInt(prompt("Attento! quello che hai inserito non è un numero!!"));
+//     }
+//     // sommo il numero alla somma precedente
+//     somma += numero;
+// }
+// // stampo la somma
+// alert("ciao la tua somma è " + somma)
 
 
 
 // -----------------------------WHILE----------------------------
+
+// creo una variabile che conterrà la somma
+var somma = 0;
+var i = 0;
+
+// ciclo per 5 volte
+while (i < 5) {
+    // chiedo il numero all'utente
+    var numero = parseInt(prompt("Inserisci il "+ (i+1) + " numero: "));
+    // valido l'input dell'utente
+    while ( isNaN(numero)) {
+        numero = parseInt(prompt("Attento! quello che hai inserito non è un numero!!"));
+    }
+    // sommo il numero alla somma precedente
+    somma += numero;
+    i++;
+}
+
+// stampo la somma
+alert("ciao la tua somma è " + somma)
