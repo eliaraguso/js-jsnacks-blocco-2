f197170cd82b515ef6c1953eb3b379752bff8559
eliaraguso <elia.raguso@gmail.com>
2021-08-27 12:41:29 +0200

    aggiunta validazione snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,11 +2,18 @@
 // se è dispari stampa il numero successivo
 
 // chiedo all'utente di inserire  un  numero 
-var inputNumero = parseInt(prompt("Inserisci un numero"))
+var inputNumero = parseInt(prompt("Inserisci un numero"));
+
+// valido l'input dell' utente
+while (isNaN(inputNumero)) {
+    inputNumero = parseInt(prompt("Questo non è un numero. Inserisci un numero!!!!"));
+}
 
 // se il numero è pari lo stampo
 if (inputNumero%2 == 0) {
-    alert("il tuo numero" + inputNumero + "è pari")
+    alert("il tuo numero " + inputNumero + " è pari");
+
+    // se non è pari stampo il successivo
 } else {
-    alert("Il tuo numero è dispari. Meglio che stampiamo il numero successivo:" + (inputNumero+1) )
+    alert("Il tuo numero è dispari. Meglio che stampiamo il numero successivo: " + (inputNumero + 1));
 }
