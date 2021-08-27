c38ec56aa585ec514e9494c82494e42ad7ad53ce
eliaraguso <elia.raguso@gmail.com>
2021-08-27 18:12:16 +0200

    fix forma snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,6 +1,8 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
+
+
 // Creo i due array con un numero di elementi diversi
 var array1 = [0, 1, 2, 3, 4];
 var array2 = [0, 1];
@@ -8,8 +10,10 @@ var array2 = [0, 1];
 alert("Ehi, non vedi che il primo array" + " " + array1 + " " + "ha un numero di elementi maggiore del secondo array?" + " " + array2);
 alert("Tranquillo, non tutto è perduto. Ci penso io!!!!!");
 
+// Eseguo un ciclo per aggiungere elementi al secondo array fintanto che i due array non avranno lo stesso numero di elementi
 while(array1.length > array2.length) {
-    array2.push("a");
+    array2.push(Math.floor(Math.random() * 100));
 }
 
-alert(array2);
+// Stampo i due array
+alert("Hai visto? Ora sei in salvo!!! Il primo array con questi elementi:" + " " + array1 + " " + "ha lo stesso numero di elementi del secondo array!!! Se non ci credi ecco gli elementi che ho creato:" + " " + array2);
